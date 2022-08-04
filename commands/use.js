eccfc3397e8220b1a083a7593d7bb7476847339d
jts234@byu.edu <jts234@byu.edu>
2022-08-04 10:47:57 -0600

    added new commands

--- commands/use.js
+++ commands/use.js
@@ -22,8 +22,9 @@ async function use (useVersion) {
         chalk.white.bold(`terraform ${useVersion} is not installed. Type "tfvm list" to see what is installed.`)
       )
     } else {
+      console.log(terraformDir)
       const programFiles = await fs.readdir(programFilesDir)
-      if (programFiles.includes(terraformDir)) {
+      if (programFiles.includes('terraform')) {
         await fs.rmdir(terraformDir)
       }
 
