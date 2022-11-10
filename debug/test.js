0dc04c693620270aa12bde346dfe36cf2acc83e5
Caleb Hill <chill389cc@gmail.com>
2022-11-10 11:37:26 -0700

    augment test file

--- debug/test.js
+++ debug/test.js
@@ -1,7 +1,12 @@
-import use from '../commands/use.js'
+import list from './../commands/list.js'
+import current from './../commands/current.js'
+import uninstall from './../commands/uninstall.js'
+import use from './../commands/use.js'
+import install from './../commands/install.js'
+import config from './../commands/config.js'
 
 async function main() {
-  await use('1.0.11')
+  await use('1.3.4')
 }
 
 main()
