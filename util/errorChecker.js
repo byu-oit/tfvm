a8f7b6c370c2c5c416e589e02d14bd19283443c6
Caleb Hill <chill389cc@gmail.com>
2023-06-01 13:27:54 -0600

    feat: publish in @byu-oit registry

--- util/errorChecker.js
+++ util/errorChecker.js
@@ -3,7 +3,7 @@ import { logger, printDebugInfo } from './logger.js'
 function getErrorMessage (error) {
   printDebugInfo()
   if (error.code === 'EPERM') {
-    // remove on next version if we don't see this anymore
+    // TODO remove on next version if we don't see this anymore
     logger.error(error, 'Unexpected elevation requirement error: ')
     console.log(
       chalk.red.bold('tfvm has to be ran from the console as an administrator for changing versions.')
