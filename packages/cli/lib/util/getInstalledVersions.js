b352d872e184085849793b716cbc6ec928548456
Caleb Hill <chill389cc@gmail.com>
2023-06-07 11:12:12 -0600

    feat: store files in appdata (#54)

--- packages/cli/lib/util/getInstalledVersions.js
+++ packages/cli/lib/util/getInstalledVersions.js
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises'
-import versionRegEx from './versionRegEx.js'
+import { versionRegEx } from './constants.js'
 import getDirectoriesObj from './getDirectoriesObj.js'
 import { logger } from './logger.js'
 
@@ -7,7 +7,7 @@ async function getInstalledVersions () {
   const directoriesObj = getDirectoriesObj()
   const tfList = []
 
-  const files = await fs.readdir(directoriesObj.tfvmDir)
+  const files = await fs.readdir(directoriesObj.tfVersionsDir)
   if (files && files.length) {
     files.forEach(file => {
       if (versionRegEx.test(file)) {
