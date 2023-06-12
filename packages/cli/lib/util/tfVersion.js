24ca4c9fdac0493972f276d0d2208a78980b00b2
Caleb Hill <chill389cc@gmail.com>
2023-06-12 10:52:46 -0600

    fix: robustly extract version (#56)
    This method improves the robustness of how we are extracting the current
    terraform version from the output of `terraform -v` by using RegEx.

--- packages/cli/lib/util/tfVersion.js
+++ packages/cli/lib/util/tfVersion.js
@@ -1,15 +1,22 @@
 import runShell from './runShell.js'
 import { logger } from './logger.js'
+import { tfCurrVersionRegEx } from './constants.js'
 
 async function getTerraformVersion () {
-  let response = (await runShell('terraform -v'))
+  const response = (await runShell('terraform -v'))
   if (response == null) {
     logger.error('Error getting terraform version')
     return null
   }
-  response = response.split('\n')[0]
-  response = response.split(' ')[1]
-  return response
+  const versionExtractionResult = Array.from(response.matchAll(tfCurrVersionRegEx))
+  if (versionExtractionResult.length === 0) {
+    logger.error('Error extracting terraform version where this is the response from `terraform -v`:\n' + response)
+    return null
+  }
+  // Terraform prints warnings at the start of the output which may contain other tf versions, such as the latest version
+  // Terraform will print the actual current version at the end of the output.
+  // Therefore, we want to grab the last match in the string and return that as our best guess for the current version
+  return versionExtractionResult[versionExtractionResult.length - 1][0]
 }
 
 export default getTerraformVersion
