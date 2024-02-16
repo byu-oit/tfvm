0ab23b78056d04651a48663c669099bca5ec4271
jsterner30 <jts234@byu.edu>
2024-02-16 11:33:12 -0700

    fix: stop the hanging after failed downlaod

--- packages/cli/lib/util/download.js
+++ packages/cli/lib/util/download.js
@@ -1,46 +1,16 @@
-import * as https from 'https'
-import * as http from 'http'
 import chalk from 'chalk'
-import fs from 'node:fs'
-import { logger } from './logger.js'
+import axios from 'axios'
+import fs from 'node:fs/promises'
 
-async function download (url, filePath, version) {
-  const proto = !url.charAt(4).localeCompare('s') ? https : http
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(filePath)
-    let fileInfo = null
-
-    const request = proto.get(url, response => {
-      if (response.statusCode !== 200) {
-        fs.unlink(filePath, () => {
-          console.log(chalk.red.bold(`Terraform ${version} is not yet released or available.`))
-        })
-        return
-      }
-
-      fileInfo = {
-        mime: response.headers['content-type'],
-        size: parseInt(response.headers['content-length'], 10)
-      }
-
-      response.pipe(file)
-    })
-
-    // The destination stream is ended by the time it's called
-    file.on('finish', () => resolve(fileInfo))
-
-    request.on('error', err => {
-      logger.error(err, `Error on request while downloading file where file=${file}, url=${url}, and version=${version}:`)
-      fs.unlink(filePath, () => reject(err))
-    })
-
-    file.on('error', err => {
-      logger.error(err, `Error on file while downloading file where file=${file}, url=${url}, and version=${version}:`)
-      fs.unlink(filePath, () => reject(err))
-    })
-
-    request.end()
-  })
+const download = async (url, filePath, version) => {
+  try {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const fileData = Buffer.from(response.data, 'binary');
+    await fs.writeFile(filePath, fileData)
+  } catch (err) {
+    console.log(chalk.red.bold(`Terraform ${version} is not yet released or available.`))
+    throw new Error()
+  }
 }
 
 export default download
